db3f96c0483eae0926208ef09f1fbaed18113bcc
Matt Stein <45152940+mattman2864@users.noreply.github.com>
2024-08-17 16:39:29 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,293 @@
+import pygame as pg
+import random
+import spaceships
+import math
+import copy
+
+# Resolution of school laptops
+RESOLUTION = (1366, 768)
+
+# Initializing pygame
+pg.init()
+
+# Initializing screen and clock
+screen = pg.display.set_mode(RESOLUTION)
+clock = pg.time.Clock()
+
+# Custom font template using coordingates. Must be in a 8x16 box
+LETTERS = {'a':[[0, 16], [4, 0], [8, 16], [6, 8], [2, 8]],
+           'b':[[0, 16], [0, 0], [6, 4], [0, 8], [6, 12], [0, 16]],
+           'c':[[8, 0], [0, 6], [0, 10], [8, 16]],
+           'd':[[0, 16], [8, 8], [0, 0], [0, 16]],
+           'e':[[8, 0], [0, 0], [0, 8], [8, 8], [0, 8], [0, 16], [8, 16]],
+           'f':[[8, 0], [0, 0], [0, 8], [4, 8], [0, 8], [0, 16]],
+           'g':[[8, 0], [0, 8], [8, 16], [8, 8], [4, 8]],
+           'h':[[0, 0], [0, 16], [0, 8], [8, 8], [8, 0], [8, 16]],
+           'i':[[0, 0], [8, 0], [4, 0], [4, 16], [0, 16], [8, 16]],
+           'j':[[0, 0], [8, 0], [6, 0], [6, 12], [3, 16], [0, 12]],
+           'k':[[0, 0], [0, 8], [8, 0], [0, 8], [8, 16], [0, 8], [0, 16]],
+           'l':[[0, 0], [0, 16], [8, 16]],
+           'm':[[0, 16], [0, 0], [4, 8], [8, 0], [8, 16]],
+           'n':[[0, 16], [0, 0], [8, 16], [8, 0]],
+           'o':[[0, 16], [8, 16], [8, 0], [0, 0], [0, 8], [0, 16]],
+           'p':[[0, 16], [0, 0], [8, 4], [0, 8], [0, 16]],
+           'q':[[4, 0], [0, 8], [4, 16], [6, 12], [8, 16], [6, 12], [8, 8], [4, 0]],
+           'r':[[0, 16], [0, 0], [8, 4], [4, 6], [8, 16], [4, 6], [0, 8]],
+           's':[[8, 0], [0, 6], [8, 12], [0, 16]],
+           't':[[0, 0], [8, 0], [4, 0], [4, 16]],
+           'u':[[0, 0], [2, 16], [6, 16], [8, 0]],
+           'v':[[0, 0], [4, 16], [8, 0]],
+           'w':[[0, 0], [2, 16], [4, 10], [6, 16], [8, 0]],
+           'x':[[0, 0], [8, 16], [4, 8], [8, 0], [0, 16]],
+           'y':[[0, 0], [4, 8], [8, 0], [4, 8], [4, 16]],
+           'z':[[0, 0], [8, 0], [0, 16], [8, 16]],
+           '0':[[0, 16], [8, 16], [8, 0], [0, 0], [0, 8], [0, 16], [8, 0]],
+           '1':[[0, 4], [4, 0], [4, 16], [0, 16], [8, 16]],
+           '2':[[0, 4], [4, 0], [8, 4], [0, 16], [8, 16]],
+           '3':[[0, 0], [8, 3], [8, 8], [0, 8], [8, 8], [8, 13], [0, 16]],
+           '4':[[8, 16], [8, 0], [0, 8], [8, 8]],
+           '5':[[8, 0], [0, 0], [0, 6], [6, 6], [8, 8], [8, 14], [6, 16], [0, 16]],
+           '6':[[8, 0], [0, 2], [0, 16], [8, 16], [8, 8], [0, 8]],
+           '7':[[0, 0], [8, 0], [0, 16]],
+           '8':[[8, 8], [0, 8], [0, 16], [8, 16], [8, 0], [0, 0], [0, 8]],
+           '9':[[0, 16], [8, 14], [8, 0], [0, 0], [0, 8], [8, 8]],
+           '.':[[2, 16], [2, 14]],
+           '?':[[0, 0], [8, 3], [4, 8], [4, 12]],
+           '(':[[6, 0], [2, 4], [2, 12], [6, 16]],
+           ')':[[2, 0], [6, 4], [6, 12], [2, 16]],
+           '-':[[0, 8], [8, 8]],
+           }
+
+def generate_font(size):
+    '''
+    Customizes font template to specific size and returns new font.
+    '''
+    l = copy.deepcopy(LETTERS)
+    for coords in l.values():
+        for coord in coords:
+            coord[0] *= size
+            coord[1] *= size
+    return l
+
+def generate_text(text, font, size, topleft, color, screen, boldness=4):
+    h = 0
+    v = 0
+    if topleft[0] == 'centered':
+        topleft[0] = RESOLUTION[0]/2 - (len(text.split("\\")[0])*size*1.4)/2
+    for letter in text:
+        if letter == '\\': 
+            v += 1
+            h = 0
+        if letter == ' ':
+            h += 1
+        if letter.lower() in font.keys():
+            pts = [[topleft[0] + p[0] + h*size*1.4, topleft[1] + p[1] + v*size*2.4] for p in font[letter.lower()]]
+            pg.draw.lines(screen, color, False, pts, boldness)
+            h += 1
+
+def generate_stars(num):
+    '''
+    Creates list of stars with random position & velocity
+    '''
+    stars = []
+    for i in range(100):
+        stars.append([random.randint(0, RESOLUTION[0]), random.randint(0, RESOLUTION[1]), random.randint(1, 5)/10])
+    return stars
+    
+def draw_stars(stars, screen):
+    '''
+    Displays the stars to the screen
+    '''
+    for star in stars:
+        pg.draw.circle(screen, '#aaaaaa', (star[0], star[1]), random.randint(20, 30)/10) # Random size for twinkle effect
+        star[0] = star[0]%RESOLUTION[0] + star[2]
+        star[1] = star[1]%RESOLUTION[1] - star[2]
+    return stars
+
+# Generating stars and fonts before game starts
+stars = generate_stars(100)
+fontsize = 16
+font = generate_font(fontsize/8)
+title_font_size = 64
+title_font = generate_font(title_font_size/8)
+score = [0, 0]
+
+'''
+START = Main menu
+SELECT = Select ship menu
+GAME = Main game loop
+PLAYAGAIN = "Play Again?" prompt screen
+QUIT = Closes the game
+'''
+running = 'START'
+while running != 'QUIT':
+    match running:
+        case 'GAME':
+            # Game Loop
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    running = 'QUIT'
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_DOWN and ship2.shooting_type == 'tap':
+                        ship2.shoot()
+                    if event.key == pg.K_s and ship1.shooting_type == 'tap':
+                        ship1.shoot()
+
+            # Right ship controls
+            if pg.key.get_pressed()[pg.K_UP]:
+                ship2.fly_forward()
+            if pg.key.get_pressed()[pg.K_LEFT]:
+                ship2.fly_turn(-1)
+            if pg.key.get_pressed()[pg.K_RIGHT]:
+                ship2.fly_turn(1)
+            if pg.key.get_pressed()[pg.K_s]:
+                if ship1.shooting_type == 'hold':
+                    ship1.shoot()
+            if pg.key.get_pressed()[pg.K_DOWN]:
+                if ship2.shooting_type == 'hold':
+                    ship2.shoot()
+            
+            # Left ship controls
+            if pg.key.get_pressed()[pg.K_w]:
+                ship1.fly_forward()
+            if pg.key.get_pressed()[pg.K_a]:
+                ship1.fly_turn(-1)
+            if pg.key.get_pressed()[pg.K_d]:
+                ship1.fly_turn(1)
+
+            # Check for game over   
+            if ship1.health <= 0:
+                running = "PLAYAGAIN"
+                score[1] += 1
+            elif ship2.health <= 0:
+                running  = "PLAYAGAIN"
+                score[0] += 1
+            
+            # Drawing to screen
+            screen.fill("black")
+
+            stars = draw_stars(stars, screen)
+
+            ship1.update() # Updates ship1 position
+            ship1.draw()
+
+            ship2.update() # Updates ship2 position
+            ship2.draw()
+
+            
+            cd = spaceships.checkdamage(ship1, ship2)
+            cooldown = [max(0, c - 1) for c in cooldown]
+            if cd[1] > 0:
+                damage_accumulator[cd[0]] += cd[1]
+                if cd[1] > 0: cooldown[cd[0]] = 30
+            for i in range(len(damage_accumulator)):
+                if cooldown[i] > 0:
+                    generate_text(str(round(damage_accumulator[i])), font, fontsize, (ship2 if i else ship1).position+pg.Vector2(40, -40), 'red', screen)
+                else: damage_accumulator[i] = 0
+
+
+            generate_text(f'{score[0]} - {score[1]}', font, fontsize, ['centered', 5], 'yellow', screen)
+        case 'PLAYAGAIN':
+            # Prompt to play again
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    running = 'QUIT'
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_y:
+                        running = 'GAME'
+                        ship1 = ship_choices[chosen[0]]((200, 359), 0, 'red', screen)
+                        ship2 = ship_choices[chosen[1]]((1366-200, 359), -math.pi, 'blue', screen)
+                        damage_accumulator = [0, 0]
+                        cooldown = [0, 0]
+                    if event.key == pg.K_n:
+                        running = 'QUIT'
+
+            # Draw sky
+            screen.fill('black')
+            stars = draw_stars(stars, screen)
+            
+            # Draws play again text
+            generate_text(f'{score[0]} - {score[1]}', title_font, title_font_size, ['centered', 50], 'RED', screen, boldness=12)
+            generate_text(f'WOULD YOU LIKE TO PLAY AGAIN?\\\\\\   yes (y)         no (n)', font, fontsize, ['centered', 300], 'yellow', screen)
+        
+        case 'START':
+            # Main menu
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    running = 'QUIT'
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_SPACE:
+                        running = 'SELECT'
+                        ship_choices = [spaceships.Alpha, spaceships.Beta]
+                        chosen = [0, 0]
+                        ready = [0, 0]
+                    if event.key == pg.K_q:
+                        running = 'QUIT'
+
+            # Draw sky
+            screen.fill('black')
+            stars = draw_stars(stars, screen)
+
+            # Draw main menu text
+            generate_text('SPACESHIPS', title_font, title_font_size, [250, 50], 'RED', screen, boldness=12)
+            generate_text('\\(SPACE) TO PLAY\\\\  (Q) TO QUIT', font, fontsize, [520, 200], 'yellow', screen)
+        case 'SELECT':
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    running = 'QUIT'
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_SPACE and ready[0] and ready[1]:
+                        running = 'GAME'
+                        ship1 = ship_choices[chosen[0]]((200, 359), 0, 'red', screen)
+                        ship2 = ship_choices[chosen[1]]((1366-200, 359), -math.pi, 'blue', screen)
+                        damage_accumulator = [0, 0]
+                        cooldown = [0, 0]
+                    if not ready[0]:
+                        if event.key == pg.K_a:
+                            chosen[0] = (chosen[0]-1)%len(ship_choices)
+                        if event.key == pg.K_d:
+                            chosen[0] = (chosen[0]+1)%len(ship_choices)
+                    if not ready[1]:
+                        if event.key == pg.K_LEFT:
+                            chosen[1] = (chosen[1]-1)%len(ship_choices)
+                        if event.key == pg.K_RIGHT:
+                            chosen[1] = (chosen[1]+1)%len(ship_choices)
+                    if event.key == pg.K_s:
+                        ready[0] = 1
+                    if event.key == pg.K_DOWN:
+                        ready[1] = 1
+
+            # Draw sky
+            screen.fill('black')
+            stars = draw_stars(stars, screen)
+            
+            generate_text('CHOOSE YOUR SHIP', font, fontsize, ['centered', 50], 'yellow', screen)
+            if not(ready[0] and ready[1]):
+                generate_text('PRESS [DOWN] TO READY', font, fontsize, ['centered', 150], 'green', screen)
+            else:
+                generate_text('[SPACE] TO START', font, fontsize, ['centered', 150], 'green', screen)
+
+            if ready[0]:
+                pg.draw.lines(screen, 'green', False, [((RESOLUTION[0]/4-20, RESOLUTION[1]/2+80)), (RESOLUTION[0]/4, RESOLUTION[1]/2+100), (RESOLUTION[0]/4+50, RESOLUTION[1]/2+50)], 10)
+            if ready[1]:
+                pg.draw.lines(screen, 'green', False, [((RESOLUTION[0]/4*3-20, RESOLUTION[1]/2+80)), (RESOLUTION[0]/4*3, RESOLUTION[1]/2+100), (RESOLUTION[0]/4*3+50, RESOLUTION[1]/2+50)], 10)
+
+            display_left = ship_choices[chosen[0]](pg.Vector2(RESOLUTION[0]/4, RESOLUTION[1]/2), -math.pi/2, 'red', screen)
+            display_left.fire = True
+            display_right = ship_choices[chosen[1]](pg.Vector2(RESOLUTION[0]/4*3, RESOLUTION[1]/2), -math.pi/2, 'blue', screen)
+            display_right.fire = True
+
+            display_left.draw()
+            display_right.draw()
+
+            pg.draw.polygon(screen, 'yellow', [[RESOLUTION[0]/4-75, RESOLUTION[1]/2], [RESOLUTION[0]/4-60, RESOLUTION[1]/2+15], [RESOLUTION[0]/4-60, RESOLUTION[1]/2-15]])
+            pg.draw.polygon(screen, 'yellow', [[RESOLUTION[0]/4+75, RESOLUTION[1]/2], [RESOLUTION[0]/4+60, RESOLUTION[1]/2+15], [RESOLUTION[0]/4+60, RESOLUTION[1]/2-15]])
+
+            pg.draw.polygon(screen, 'yellow', [[RESOLUTION[0]/4*3-75, RESOLUTION[1]/2], [RESOLUTION[0]/4*3-60, RESOLUTION[1]/2+15], [RESOLUTION[0]/4*3-60, RESOLUTION[1]/2-15]])
+            pg.draw.polygon(screen, 'yellow', [[RESOLUTION[0]/4*3+75, RESOLUTION[1]/2], [RESOLUTION[0]/4*3+60, RESOLUTION[1]/2+15], [RESOLUTION[0]/4*3+60, RESOLUTION[1]/2-15]])
+
+
+
+    # 60 FPS
+    clock.tick(60)
+    pg.display.update()
---
+++ spaceships.py
@@ -0,0 +1,286 @@
+import pygame as pg
+import math
+import random
+
+def sign(num):
+    if num == 0:
+        return 1
+    return abs(num)/num
+
+def rotate_poly(points, angle):
+    rotated_points = []
+    for p in points:
+        dist = math.dist((0, 0), p)
+        a = math.atan(p[1] / (p[0]+ 0.0000000001)) + (math.pi if p[0] < 0 else 0)
+        rotated_points.append((dist*math.cos(angle + a), dist*math.sin(angle + a)))
+    return rotated_points
+
+def place_poly(points, center):
+    new_points = []
+    for p in points:
+        new_points.append((p[0] + center[0], p[1] + center[1]))
+    return new_points
+
+def draw_on_ship(points, angle, position, color, screen):
+    adjusted_points = place_poly(rotate_poly(points, angle), position)
+    pg.draw.polygon(screen, color, adjusted_points, width=3)
+
+def clamp(n, smallest, largest): return max(smallest, min(n, largest))
+
+class Spaceship():
+    def __init__(self, starting_coords, starting_angle, color, screen, shooting_type) -> None:
+        super().__init__()
+        self.position = pg.Vector2(starting_coords)
+        self.angle = starting_angle
+        self.color = color
+        self.velocity = pg.Vector2(0, 0)
+        self.angular_velocity = 0
+        self.screen = screen
+        self.fire = False
+        self.bullets = []
+        self.health = 100
+        self.damage_decay = 0
+        self.damage_taken = 0
+        self.shooting_type = shooting_type
+        self.stun = 0
+    
+    def fly_forward(self):
+        if self.stun > 0:
+            return
+        MAX_VEL = 10
+        accel = 0.2
+        self.velocity += pg.Vector2(accel * math.cos(self.angle), accel * math.sin(self.angle)) 
+        if math.dist((0, 0), self.velocity) > MAX_VEL:
+            self.velocity.scale_to_length(MAX_VEL)
+        
+        # Draw Fire
+        self.fire = True
+
+    
+    def fly_turn(self, dir):
+        if self.stun > 0:
+            return
+        MAX_ANG_VEL = 0.05
+        accel = 0.002
+        self.angular_velocity += accel * dir
+        if abs(self.angular_velocity) > MAX_ANG_VEL:
+            self.angular_velocity = MAX_ANG_VEL * dir
+    
+    def update(self):
+        if self.damage_decay > 0:
+            self.damage_decay -= 1
+        elif self.damage_taken > 0:
+            self.damage_taken -= 1
+        self.hitbox = [
+            pg.Vector2(30*math.cos(self.angle), 30*math.sin(self.angle)) + pg.Vector2(17*math.cos(self.angle + math.pi/2), 17*math.sin(self.angle + math.pi/2)) + self.position,
+            pg.Vector2(30*math.cos(self.angle), 30*math.sin(self.angle)) + pg.Vector2(17*math.cos(self.angle - math.pi/2), 17*math.sin(self.angle - math.pi/2)) + self.position,
+            pg.Vector2(-30*math.cos(self.angle), -30*math.sin(self.angle)) + pg.Vector2(-17*math.cos(self.angle + math.pi/2), -17*math.sin(self.angle + math.pi/2)) + self.position,
+            pg.Vector2(-30*math.cos(self.angle), -30*math.sin(self.angle)) + pg.Vector2(-17*math.cos(self.angle - math.pi/2), -17*math.sin(self.angle - math.pi/2)) + self.position,
+
+        ]
+        pg.draw.polygon(self.screen, 'green', self.hitbox, 3)
+        self.velocity[0] *= 0.98
+        self.velocity[1] *= 0.98
+        self.angular_velocity *= 0.98
+        if abs(self.angular_velocity) < 0.0001:
+            self.angular_velocity = 0
+        if abs(self.velocity[0]) < 0.001:
+            self.velocity[0] = 0
+        if abs(self.velocity[1]) < 0.001:
+            self.velocity[1] = 0
+        self.position[0] += self.velocity[0]
+        self.position[1] += self.velocity[1]
+        self.angle += self.angular_velocity
+        self.position[0] %= 1366
+        self.position[1] %= 768
+        self.stun = max(0, self.stun - 1)
+        for bullet in self.bullets:
+            bullet.update()
+            if bullet.clock < 0: self.bullets.remove(bullet)
+
+    def draw_health(self):
+        flip = 1 if self.position[1] > 55 else -1
+        pg.draw.line(self.screen, '#555555', (clamp(-50+self.position[0], 0, 1366-100), -50*flip+self.position[1]), 
+                     (clamp(50+self.position[0], 100, 1366), -50*flip+self.position[1]), 10)
+        if self.damage_taken > 0:
+            pg.draw.line(self.screen, pg.Color(255, 200, 200), (clamp(-50+self.position[0], 0, 1366-100), -50*flip+self.position[1]), 
+                     (clamp(-50+self.health+self.damage_taken+self.position[0], 0+self.health+self.damage_taken, 1366-100+self.health+self.damage_taken), -50*flip+self.position[1]), 10)
+        pg.draw.line(self.screen, pg.Color(clamp(int(255-self.health*2.55), 0, 255), clamp(int(self.health*2.55), 0, 255), 0), (clamp(-50+self.position[0], 0, 1366-100), -50*flip+self.position[1]), 
+                     (clamp(-50+self.health+self.position[0], self.position[0]-50, 1366-100+self.health), -50*flip+self.position[1]), 10)
+        
+        if self.stun > 0:
+            charge = []
+            detail = 10
+            for i in range(detail):
+                mult = random.randint(2, 30)
+                charge.append((mult*math.cos(i*math.pi/detail*2), mult*math.sin(i*math.pi/detail*2)))
+            draw_on_ship(charge, self.angle, self.position, 'yellow', self.screen)
+
+        
+class Alpha(Spaceship):
+    def __init__(self, starting_coords, starting_angle, color, screen) -> None:
+        super().__init__(starting_coords, starting_angle, color, screen, 'tap')
+        self.cannon_charge = 0
+        self.CANNON_MAX = 50
+    def update(self):
+        if not self.stun > 0:
+            self.cannon_charge += 1
+        return super().update()
+    def shoot(self):
+        if self.cannon_charge < self.CANNON_MAX or self.stun > 0:
+            return
+        self.bullets.append(Charge(self.position, self.velocity, self.angle, self.screen))
+        self.cannon_charge = 0
+    def draw(self):
+        ship = [(30, 16), (17, 12), (3, 4), (-3, 4), (-17, 12), (-30, 4), (-25, 0),
+                (-30, -4), (-17, -12), (-3, -4), (3, -4), (17, -12), (30, -16), 
+                (18, -6), (15, 0), (18, 6)]
+        draw_on_ship(ship, self.angle, self.position, self.color, self.screen)
+
+        if not self.stun > 0:
+            charge = []
+            filled = min(1, self.cannon_charge / self.CANNON_MAX)
+            for i in range(10):
+                mult = random.randint(5, 10) * filled
+                charge.append((mult*math.cos(i*math.pi/5) + 30, mult*math.sin(i*math.pi/5)))
+            draw_on_ship(charge, self.angle, self.position, pg.Color(255, 255, int(255-255*filled)), self.screen)
+
+        if self.fire:        
+            fire = [(-30, 4), (-50 + random.randint(-3, 3), random.randint(-3, 3)), (-30, -4), (-25, 0)]
+            draw_on_ship(fire, self.angle, self.position, 'orange', self.screen)
+            self.fire = False
+        return super().draw_health()
+
+class Beta(Spaceship):
+    def __init__(self, starting_coords, starting_angle, color, screen) -> None:
+        super().__init__(starting_coords, starting_angle, color, screen, 'hold')
+        self.cannon_cooldown = 10
+    def update(self):
+        self.cannon_cooldown = max(self.cannon_cooldown - 1, 0)
+        return super().update()
+    def shoot(self):
+        if self.cannon_cooldown > 0 or self.stun > 0:
+            return
+        self.cannon_cooldown = 10
+        self.bullets.append(Bullet(self.position, self.velocity, self.angle, -1, self.screen))
+        self.bullets.append(Bullet(self.position, self.velocity, self.angle, 1, self.screen))
+    def draw(self):
+        ship = [[-30, 10], [-15, 17], [4, 12], [-15, 6], [15, 2], [20, 6], [30, 2], [30, -2], [20, -6], [15, -2], [-15, -6], [4, -12], [-15, -17], [-30, -10], [-20, 0]]
+        draw_on_ship(ship, self.angle, self.position, self.color, self.screen)
+        
+        if self.fire:        
+            fire = [(-25, -5), (-50 + random.randint(-3, 3), random.randint(-3, 3)), (-25, 5), (-20, 0)]
+            draw_on_ship(fire, self.angle, self.position, 'orange', self.screen)
+            self.fire = False
+        return super().draw_health()
+class Gamma(Spaceship):
+    def __init__(self, starting_coords, starting_angle, color, screen, shooting_type) -> None:
+        super().__init__(starting_coords, starting_angle, color, screen, shooting_type)
+        self.charge = 0
+        self.max_charge = 100
+    def update(self):
+        if self.stun > 0:
+            return
+        self.charge = min(self.max_charge, self.charge + 1)
+    def shoot(self):
+        if self.charge < self.max_charge or self.stun > 0:
+            return
+        
+
+
+
+
+class Charge:
+    def __init__(self, position, velocity, angle, screen):
+        self.position = pg.Vector2(position) + pg.Vector2(20*math.cos(angle), 20*math.sin(angle))
+        speed = 20
+        self.velocity = pg.Vector2(velocity) + pg.Vector2(speed*math.cos(angle), speed*math.sin(angle))
+        self.angle = angle
+        self.screen = screen
+        self.clock = 100
+        self.maxclock = self.clock
+        self.size = 1
+        self.damage = self.size * 8
+    def update(self):
+        self.clock -= 1
+        self.size += 0.1
+        self.damage = self.size * 8
+        self.position[0] += self.velocity[0]
+        self.position[1] += self.velocity[1]
+        self.draw()
+        l = self.size * 10
+        w = self.size * 10
+        self.hitbox = [
+            pg.Vector2(l*math.cos(self.angle), l*math.sin(self.angle)) + pg.Vector2(w*math.cos(self.angle + math.pi/2), w*math.sin(self.angle + math.pi/2)) + self.position,
+            pg.Vector2(l*math.cos(self.angle), l*math.sin(self.angle)) + pg.Vector2(w*math.cos(self.angle - math.pi/2), w*math.sin(self.angle - math.pi/2)) + self.position,
+            pg.Vector2(-l*math.cos(self.angle), -l*math.sin(self.angle)) + pg.Vector2(-w*math.cos(self.angle + math.pi/2), -w*math.sin(self.angle + math.pi/2)) + self.position,
+            pg.Vector2(-l*math.cos(self.angle), -l*math.sin(self.angle)) + pg.Vector2(-w*math.cos(self.angle - math.pi/2), -w*math.sin(self.angle - math.pi/2)) + self.position,
+
+        ]
+        pg.draw.polygon(self.screen, 'green', self.hitbox, 3)
+
+    def draw(self):
+        charge = []
+        detail = 10
+        for i in range(detail):
+            mult = random.randint(5, 10) * self.size
+            charge.append((mult*math.cos(i*math.pi/detail*2), mult*math.sin(i*math.pi/detail*2)))
+        draw_on_ship(charge, self.angle, self.position, 'yellow', self.screen)
+
+class Bullet():
+    def __init__(self, position, velocity, angle, side, screen):
+        self.position = pg.Vector2(position) + pg.Vector2(-10*math.cos(angle), -10*math.sin(angle)) + pg.Vector2(12*math.cos(angle+math.pi/2 * side), 12*math.sin(angle+math.pi/2*side))
+        speed = 20
+        self.velocity = pg.Vector2(velocity) + pg.Vector2(speed*math.cos(angle), speed*math.sin(angle))
+        self.angle = angle
+        self.screen = screen
+        self.clock = 100
+        self.maxclock = self.clock
+        self.damage = 2
+        self.size = 2
+    def update(self):
+        self.clock -= 1
+        self.position[0] += self.velocity[0]
+        self.position[1] += self.velocity[1]
+        self.draw()
+        l = 15
+        w = self.size * 3
+        self.hitbox = [
+            pg.Vector2(l*math.cos(self.angle), l*math.sin(self.angle)) + pg.Vector2(w*math.cos(self.angle + math.pi/2), w*math.sin(self.angle + math.pi/2)) + self.position,
+            pg.Vector2(l*math.cos(self.angle), l*math.sin(self.angle)) + pg.Vector2(w*math.cos(self.angle - math.pi/2), w*math.sin(self.angle - math.pi/2)) + self.position,
+            pg.Vector2(-l*math.cos(self.angle), -l*math.sin(self.angle)) + pg.Vector2(-w*math.cos(self.angle + math.pi/2), -w*math.sin(self.angle + math.pi/2)) + self.position,
+            pg.Vector2(-l*math.cos(self.angle), -l*math.sin(self.angle)) + pg.Vector2(-w*math.cos(self.angle - math.pi/2), -w*math.sin(self.angle - math.pi/2)) + self.position,
+
+        ]
+        pg.draw.polygon(self.screen, 'green', self.hitbox, 3)
+    def draw(self):
+        bullet = [[0, 3], [0, -3], [15, 0]]
+        draw_on_ship(bullet, self.angle, self.position, 'white', self.screen)
+
+
+def checkdamage(ship1: Spaceship, ship2: Spaceship):
+    for i in ship1.bullets:
+        if math.dist(i.position, ship2.position) < i.size*7.5 or\
+            math.dist(i.position, ship2.position + pg.Vector2(20*math.cos(ship2.angle), 20*math.sin(ship2.angle))) < i.size*10 or\
+            math.dist(i.position, ship2.position + pg.Vector2(-20*math.cos(ship2.angle), -20*math.sin(ship2.angle))) < i.size*10:
+            ship2.health -= i.damage
+            ship2.damage_taken += i.damage
+            ship2.damage_decay = 30
+            i.clock = 0
+            ship1.bullets.remove(i)
+            if type(i) == Charge:
+                ship2.stun = 20
+            return [1, i.damage]
+    for i in ship2.bullets:
+        if math.dist(i.position, ship1.position) < i.size*7.5 or\
+            math.dist(i.position, ship1.position + pg.Vector2(20*math.cos(ship1.angle), 20*math.sin(ship1.angle))) < i.size*10 or\
+            math.dist(i.position, ship1.position + pg.Vector2(-20*math.cos(ship1.angle), -20*math.sin(ship1.angle))) < i.size*10:
+            ship1.health -= i.damage
+            ship1.damage_taken += i.damage
+            ship1.damage_decay = 30
+            i.clock = 0
+            ship2.bullets.remove(i)
+            if type(i) == Charge:
+                ship1.stun = 20
+            return [0, i.damage]
+    return [-1, -1]
